914057e826e9b52be90d811910a5baf55e6ca034
Jorge de Oro <jorgedeoro@yahoo.com.ar>
2025-03-03 16:12:07 -0300

    modificacion imagen institucional

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
-import { View, Text, useWindowDimensions, Image, StyleSheet } from 'react-native'
+import { Image } from 'expo-image'
+import { View, Text, useWindowDimensions, StyleSheet } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
@@ -6,30 +7,48 @@ const index = () => {
   const { height, width } = useWindowDimensions();
 
   return (
-    <SafeAreaView style={{ display: 'flex', flex: 1, backgroundColor: '#008100' }}>
-
-      <Image
-        source={require('@/assets/images/os/moya.png')}
-        style = {style.StyleImagen}
-      />
-      <View
-        style={style.Fondo}
-      >
-        <Text style={{ color: 'white', marginHorizontal:4 , fontSize:20}}>Institucional</Text>
-
+    <SafeAreaView
+      style={{ display: "flex", flex: 1, backgroundColor: "#008100" }}
+    >
+      <View style={style.container}>
+        <Image
+          source={require("@/assets/images/os/moya.png")}
+          style={style.image}
+          contentFit='contain'
+          transition={1000}
+        />
       </View>
+
+      <View style={style.Fondo}>
+        <Text style={{ color: "white", marginHorizontal: 4, fontSize: 20 }}>
+          Institucional
+        </Text>
+      </View>
+
     </SafeAreaView>
   );
 }
 
 const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+
   StyleImagen : {
     backgroundImage: '#008100',
     alignSelf: 'auto',
     resizeMode: 'cover',
   },
+  image: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#0553',
+  },  
   Fondo : {
     display: 'flex',
+    flex:5,
     backgroundImage: 'repeating-linear-gradient(#008100, #000000a7 10%, green 20%)',
     paddingTop: 6,
   }
